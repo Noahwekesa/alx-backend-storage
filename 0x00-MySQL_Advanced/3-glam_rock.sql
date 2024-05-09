f722ea5e451e9c8cf2918318bd986b1de1989a4e
noahwekesa <noahwekesa0@gmail.com>
2024-05-09 15:12:50 +0300

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- sql script that lists all bands with GLam rock
-SELECT band_name, ifnull(split, 2020)- ifnull(formed, 0) AS lifespan
+SELECT band_name, ifnull(split, 2022)- ifnull(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%GLam rock%'
 ORDER BY lifespan DESC;
