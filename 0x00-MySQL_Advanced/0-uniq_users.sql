e3d06bf50989d3dc22f64f8b0852439e610f0f90
noahwekesa <noahwekesa0@gmail.com>
2024-05-09 13:31:39 +0300

    auto increment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- sql script that creates a table users.
 
 CREATE TABLE IF NOT EXISTS users (
-id int PRIMARY KEY NOT NULL,
-email varchar(255) NOT NULL UNIQUE,
-
-name varchar(255),
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email varchar(255) NOT NULL UNIQUE,
+  name varchar(255)
 );
