c18558643ff84c313180b34480c152c3453b94e3
noahwekesa <noahwekesa0@gmail.com>
2024-05-09 14:37:19 +0300

    old school bad

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- sql script that lists all bands with GLam rock
+
+SELECT band_name, ifnull(split, 2020)- ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%GLam rock%'
+ORDER BY lifespan DESC;
