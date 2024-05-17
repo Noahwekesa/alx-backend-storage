aeca463c69c3e76c5511a09de0ef018269ca9b5d
noahwekesa <noahwekesa0@gmail.com>
2024-05-17 19:04:26 +0300

    reading from redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ write string to redis
 
 import redis
 import uuid
-from typing import Union
+from typing import Optional, Union, Callable
 
 
 class Cache:
@@ -17,3 +17,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable[[bytes], Union[str, int]]] = None
+    ) -> Optional[Union[str, int]]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value.decode()
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, lambda x: x.decode())
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, lambda x: int(x))
